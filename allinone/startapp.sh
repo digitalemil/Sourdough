addd5d104d7c4823daee591c8cc89873a7a10421
Emil A. Siemes <emil@Emils-MBP.fritz.box>
2024-01-28 18:33:19 +0100

    provisioning

--- allinone/startapp.sh
+++ allinone/startapp.sh
@@ -12,9 +12,9 @@ sleep 1
 
 /opt/app/prometheus-2.49.1.linux-amd64/prometheus --web.listen-address="0.0.0.0:9090" --config.file=/opt/app/prometheus.conf &
 
-/usr/bin/loki -config.file=loki-local-config.yaml &
+/usr/bin/loki -config.file=/opt/app/loki-local-config.yaml &
 
-/usr/bin/tempo -config.file /opt/app/tempo-local-config.yaml &
+/usr/bin/tempo -config.file=/opt/app/tempo-local-config.yaml &
 
 cp /opt/app/datasources.yaml /usr/share/grafana/conf/provisioning/datasources/
 cp /opt/app/dashboard.json /var/lib/grafana/dashboards
