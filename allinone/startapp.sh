25b7706e6a77f1521edb310f23c780d37576dffc
Emil Andreas Siemes <emil@Emils-MacBook-Air.fritz.box>
2024-01-28 14:09:23 +0100

    prometheus

--- allinone/startapp.sh
+++ allinone/startapp.sh
@@ -9,6 +9,9 @@ cockroach-v*.linux-amd64/cockroach sql --insecure -f ddl.sql
 
 sleep 1
 
+
+/opt/app/prometheus-2.49.1.linux-amd64/prometheus --web.listen-address="0.0.0.0:9090" --config.file=/opt/app/prometheus.conf &
+
 node --require './tracing.js' ./bin/www 
 
 
