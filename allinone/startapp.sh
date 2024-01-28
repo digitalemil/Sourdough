e229c0634da70a964214f82ff68ddedfe690e076
Emil Andreas Siemes <emil@Emils-MacBook-Air.fritz.box>
2024-01-28 14:39:46 +0100

    grafana behind nginx

--- allinone/startapp.sh
+++ allinone/startapp.sh
@@ -12,7 +12,9 @@ sleep 1
 
 /opt/app/prometheus-2.49.1.linux-amd64/prometheus --web.listen-address="0.0.0.0:9090" --config.file=/opt/app/prometheus.conf &
 
-/usr/share/grafana/bin/grafana server &
+cd /usr/share/grafana;
+./bin/grafana server &
+cd /opt/app
 
 node --require './tracing.js' ./bin/www 
 
