f381727b57decf05bd4c6de6308296faa9573ad1
Emil Andreas Siemes <emil@Emils-MacBook-Air.fritz.box>
2024-01-28 14:24:34 +0100

    prometheus

--- allinone/startapp.sh
+++ allinone/startapp.sh
@@ -12,6 +12,8 @@ sleep 1
 
 /opt/app/prometheus-2.49.1.linux-amd64/prometheus --web.listen-address="0.0.0.0:9090" --config.file=/opt/app/prometheus.conf &
 
+/usr/share/grafana/bin/grafana server &
+
 node --require './tracing.js' ./bin/www 
 
 
