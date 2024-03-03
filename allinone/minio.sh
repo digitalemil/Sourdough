09c46faf4132312c80ab32d3237e766651ad7052
Emil A. Siemes <emil@Emils-MBP.fritz.box>
2024-03-03 18:23:38 +0100

    with minio part II

--- allinone/minio.sh
+++ allinone/minio.sh
@@ -1,8 +1,11 @@
 
-/opt/minio/minio server /mnt/data --console-address :38991 >$LOGFOLDER/minio.log 2>&1  &
+export MINIOLOGFOLDER=$LOGFOLDER/../minio
+mkdir -p $MINIOLOGFOLDER
+
+/opt/minio/minio server /mnt/data --console-address :38991 >$MINIOLOGFOLDER/minio.log 2>&1  &
 sleep 8
 
-/opt/minio/mc --insecure alias set admin http://localhost:9000/ minioadmin minioadmin  >$LOGFOLDER/mc1.out 2>&1  &
-/opt/minio/mc --insecure admin user add admin cockroachdb cockroachdb >$LOGFOLDER/mc2.out 2>&1  &
-/opt/minio/mc --insecure admin policy attach admin readwrite --user cockroachdb >$LOGFOLDER/mc3.out 2>&1  &
-/opt/minio/mc --insecure mb admin/cockroachd b>$LOGFOLDER/mc4.out 2>&1  &
+/opt/minio/mc --insecure alias set admin http://localhost:9000/ minioadmin minioadmin  >$MINIOMINIOLOGFOLDER/mc1.out 2>&1  &
+/opt/minio/mc --insecure admin user add admin cockroachdb cockroachdb >$MINIOLOGFOLDER/mc2.out 2>&1  &
+/opt/minio/mc --insecure admin policy attach admin readwrite --user cockroachdb >$MINIOLOGFOLDER/mc3.out 2>&1  &
+/opt/minio/mc --insecure mb admin/cockroachd b>$MINIOLOGFOLDER/mc4.out 2>&1  &
